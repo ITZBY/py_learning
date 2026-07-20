0e7a06452feb23a3abca45e4d94721a8360f1243
ITZBY <1173617833@qq.com>
2026-07-20 11:00:02 +0800

    删除无用文件

--- code/selenium_006(hongqiao_useful-fun).py
+++
@@ -1,8 +0,0 @@
-from selenium_005(hongqiao_login) import hognqiao_login_base
-from selenium_005(hongqiao_login) import EC
-from selenium_005(hongqiao_login) import By
-from selenium_005(hongqiao_login) import time
-
-class housingList(hognqiao_login_base):
-    def test_login(self):
-        self.login(username="zby", password="test123456")
